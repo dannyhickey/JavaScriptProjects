8770ef64c338fcc59c59d7a490509ca0c4e68c65
Daniel Hickey <daniellhickey@gmail.com>
2016-04-20 11:07:36 +0100

    Setting up and testing the colour choice display in the h1

--- ColourGuess/color.js
+++ ColourGuess/color.js
@@ -5,11 +5,16 @@ var colours = [
 		"rgb(0,0,255)",
 		"rgb(0,255,255)",
 		"rgb(255,0,255)"
-	]
+	];
 
-	var	box = document.querySelectorAll(".box");
-	/*loops through the "box" class elements and also loops through colours array
-	  assigning corresponding values in array to each element cycled through*/
-	for (var i = 0; i < box.length; i++) {
-		box[i].style.background = colours[i];
-	}
+var chosenColour = colours[3];
+
+var colourChoice = document.querySelector("#colourChoice");//Selecting the colourChoice id in the span tag of h1 element.
+colourChoice.textContent = chosenColour;
+
+var	box = document.querySelectorAll(".box");
+/*loops through the "box" class elements and also loops through colours array
+  assigning corresponding values in array to each element cycled through*/
+for (var i = 0; i < box.length; i++) {
+	box[i].style.background = colours[i];
+}
