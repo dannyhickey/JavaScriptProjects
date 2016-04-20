92ff338f14224f40c65a18a99b6be6cc0eeb2202
Daniel Hickey <daniellhickey@gmail.com>
2016-04-20 11:31:57 +0100

    adding and testing event to check if the box clicked is the sane as the required colour

--- ColourGuess/color.js
+++ ColourGuess/color.js
@@ -1,20 +1,34 @@
 var colours = [
-		"rgb(255,0,0)",
-		"rgb(255,255,0)",
-		"rgb(0,255,0)",
-		"rgb(0,0,255)",
-		"rgb(0,255,255)",
-		"rgb(255,0,255)"
+		"rgb(255, 0, 0)",
+		"rgb(255, 255, 0)",
+		"rgb(0, 255, 0)",
+		"rgb(0, 255, 255)",
+		"rgb(0, 0, 255)",
+		"rgb(255 ,0 ,255)"
 	];
 
 var chosenColour = colours[3];
 
 var colourChoice = document.querySelector("#colourChoice");//Selecting the colourChoice id in the span tag of h1 element.
 colourChoice.textContent = chosenColour;
-
 var	box = document.querySelectorAll(".box");
+
 /*loops through the "box" class elements and also loops through colours array
   assigning corresponding values in array to each element cycled through*/
 for (var i = 0; i < box.length; i++) {
 	box[i].style.background = colours[i];
+	//adding click event listener to each box.
+	box[i].addEventListener("click", function()
+	{
+		//get the colour of selected square
+		var colourClicked = this.style.background;
+		alert(colourClicked);
+		//compare the colour to the picked colour
+		if(colourClicked === chosenColour)
+			{	
+				alert("Correct!");
+			}else{
+				alert("Wrong!");
+			}
+	});
 }
