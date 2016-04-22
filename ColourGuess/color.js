72176c84ae2101b7353a36f3120437aa41e5286d
Daniel Hickey <daniellhickey@gmail.com>
2016-04-22 16:25:21 +0100

    Disabled the reset feature for now. Refactored the changeAllColours function. Will refactor more in a while after more features are added.

--- ColourGuess/color.js
+++ ColourGuess/color.js
@@ -11,7 +11,7 @@ var colours = generateRandomColours(6);
 var resetColours = document.querySelector("#resetColours");
 var msgDisplay = document.querySelector("#msgDisplay");
 var	h1 = document.querySelector("h1");
-var chosenColour = randomColour();
+var chosenColour = pickRandomColour();
 
 var colourChoice = document.querySelector("#colourChoice");//Selecting the colourChoice id in the span tag of h1 element.
 colourChoice.textContent = chosenColour;
@@ -29,7 +29,7 @@ for (var i = 0; i < box.length; i++) {
 		if(colourClicked === chosenColour)
 			{	
 				msgDisplay.textContent = "Correct!";// if correct change the message display to "Correct".
-				changeAllColours();
+				changeAllColours(colourClicked);
 							}else{
 				this.style.background = "#487A21"; // changes selected box colour to match the background colour of the body
 				msgDisplay.textContent = "Try Again!";// if incorrect change the message display to "Try Again".
@@ -38,50 +38,62 @@ for (var i = 0; i < box.length; i++) {
 }
 
 //Changes all colours  to the correct colour when the correct is selected.
-function changeAllColours(){
+function changeAllColours(colour){
 	//for loop to cycle through box classes
 	for (var i = 0; i < box.length; i++) {
-		box[i].style.background = chosenColour;
-		h1.style.background = chosenColour;
+		box[i].style.background = colour;
+		h1.style.background = colour;
 	}
-
-
-	//change colour of all box classes and h1 element to match selected colour
-	//box[i].style.background = chosenColour;
-
 }
 
 
 
-resetColours.addEventListener("click", function(){
+/*resetColours.addEventListener("click", function(){
 		
 		colourGenerate();
 		
 	});
-
-//This function generates the colours for the beginning of the game.
+*/
+/*//This function generates the colours for the beginning of the game.
 function colourGenerate(){
 
 
 			msgDisplay.textContent = "";
-			/*loops through the "box" class elements and also loops through colours array
-  			assigning corresponding values in array to each element cycled through*/
+			loops through the "box" class elements and also loops through colours array
+  			assigning corresponding values in array to each element cycled through
 			for (var i = 0; i < box.length; i++) {
 			box[i].style.background = colours[i];
 			h1.style.background = "none";// Removes the background style of the h1
 		}
 }
-
-function randomColour(){
+*/
+function pickRandomColour(){
 	var rand = Math.floor(Math.random() * colours.length);
 	return colours[rand];
 }
 
 function generateRandomColours(num)
 {
-	//make array
-
+	//make empty array
+	var arr = [];
 	//add "num" random colours to the array
+	for (var i = 0; i < num; i++) {
+		//push random colour to the array
+		arr.push(randomColour());
+	}
+	return arr;
+}
 
-	//return
+function randomColour(){
+	//choose a "red" from 0 to 255
+	var r = Math.floor(Math.random() * 256);
+	
+	//choose a "green" from 0 to 255
+	var g = Math.floor(Math.random() * 256);
+	
+	//choose a "blue" from 0 to 255
+	var b = Math.floor(Math.random() * 256);
+
+	//"rgb(r, g, b)"
+	return "rgb(" + r + "," + g + "," + b + ")";
 }
