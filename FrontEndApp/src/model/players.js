58b36c358a6bf28b0e0b8acc249dac570dca9355
Daniel Hickey <daniellhickey@gmail.com>
2016-05-10 23:23:44 +0100

    The Player.add procedure takes care of creating a new Player instance and adding it to the Player.instances collection

--- FrontEndApp/src/model/players.js
+++ FrontEndApp/src/model/players.js
@@ -5,4 +5,13 @@ function Player( slots) {
   this.dob = slots.dob;
 };
 
+
 Player.instances = {};//initialize player instances
+
+//The Player.add procedure takes care of creating a new Player instance and adding it to the Player.instances collection 
+Player.add = function (slots) {
+  var player = new Player( slots);
+  // add Player to the Player.instances collection
+  Player.instances[slots.idNum] = player;
+  console.log("Player " + slots.idNum + " created!");
+};
