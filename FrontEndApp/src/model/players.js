8868bb9a9c31ef4deb0dea76f8405faa4e1291e6
Daniel Hickey <daniellhickey@gmail.com>
2016-05-11 19:47:13 +0100

    /Retrieving the player table that has been stored as a large

--- FrontEndApp/src/model/players.js
+++ FrontEndApp/src/model/players.js
@@ -10,7 +10,7 @@ Player.instances = {};//initialize player instances
 
 //The Player.add procedure takes care of creating a new Player instance and adding it to the Player.instances collection 
 Player.add = function (slots) {
-  var player = new Player( slots);
+  var player = new Player(slots);
   // add Player to the Player.instances collection
   Player.instances[slots.idNum] = player;
   console.log("Player " + slots.idNum + " created!");
