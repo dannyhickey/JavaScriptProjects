a21c431c16f7fcc8ffc4b0b9945b969c86a2b33c
Daniel Hickey <daniellhickey@gmail.com>
2016-05-12 22:29:55 +0100

    Converting the player table string into a corresponding entity table players with player rows as elements, with the help of the built-in procedure JSON.parse

--- FrontEndApp/src/model/players.js
+++ FrontEndApp/src/model/players.js
@@ -15,3 +15,12 @@ Player.add = function (slots) {
   Player.instances[slots.idNum] = player;
   console.log("Player " + slots.idNum + " created!");
 };
+
+//Retrieving the player table that has been stored as a large 
+//string with the key "players" from Local Storage with the help of the assignment
+playersString = localStorage["players"];
+
+//Converting the player table string into a corresponding entity table 
+//players with player rows as elements, with the help of the built-in procedure JSON.parse: 
+players = JSON.parse(playersString);
+
